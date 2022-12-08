345f0685310aad5016192585a50542337a4a863c
lakpesdam1945 <lakpesdam.rck@gmail.com>
2022-12-09 00:58:15 +0700

    add variant layouts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,9 @@
-import TokohCard from "../components/Card/TokohCard";
-import WartaCard from "../components/Card/WartaCard";
-import Carousel from "../components/Carousel/Carousel";
-import { backgroundList, Container } from "../components/Container";
-import { Header } from "../components/Header/Header";
-import SepartorsContent from "../components/SepartorsContent";
+import React from "react";
 
-function Home() {
-  return (
-    <>
-      <Header />
-      <Container padding="px-4 py-20 pb-20 w-full min-h-screen">
-        <Carousel />
-        <SepartorsContent
-          theme={backgroundList.primary}
-          title="Warta"
-          ButtonIconLink="/warta"
-        />
-        <WartaCard />
-        <SepartorsContent
-          theme={backgroundList.primary}
-          title="Tokoh Nahdlatul Ulama"
-          ButtonIconLink="/tokoh"
-        />
-        <TokohCard />
-      </Container>
-    </>
-  );
+type Props = {};
+
+function Home({}: Props) {
+  return <div>Home</div>;
 }
 
-export { Home };
+export default Home;
